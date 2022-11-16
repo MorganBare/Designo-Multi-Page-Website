c7c342b20889ffe24809e8e86080617e56c95e21
Morgan <morganjbare@gmail.com>
2022-11-16 22:00:01 +0100

    Added content and svg background of footer

--- src/components/footers/Footer.jsx
+++ src/components/footers/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-import { FooterContainer } from './Footer.styled'
+import { FooterContainer, GetInTouchContainer, ContactInfoContainer, ContactNav } from './Footer.styled'
+import { DarkButton } from "../../styles/Buttons.styled.js"
+import lightLogo from "../../assets/shared/desktop/logo-light.png"
 
 export default function Footer() {
   return (
-    <FooterContainer></FooterContainer>
+    <FooterContainer>
+
+      <GetInTouchContainer>
+        <div>
+          <h3>Let's talk about your project</h3>
+          <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
+        </div>
+        <DarkButton>GET IN TOUCH</DarkButton>
+      </GetInTouchContainer>
+
+      <ContactInfoContainer>
+        <ContactNav>
+          <img src={lightLogo} alt="logo" />
+          <ul>
+            <li>OUR COMPANY</li>
+            <li>LOCATIONS</li>
+            <li>CONTACT</li>
+          </ul>
+        </ContactNav>
+      </ContactInfoContainer>
+
+    </FooterContainer>
   )
 }
--- src/components/footers/Footer.styled.js
+++ src/components/footers/Footer.styled.js
@@ -1,4 +1,31 @@
 import styled from "styled-components";
 
+import bgMobile from "../../assets/shared/desktop/bg-pattern-call-to-action.svg"
+
 export const FooterContainer = styled.div`
+
+    position: relative;
+
 `;
+
+export const GetInTouchContainer = styled.div`
+    background-image: url('${bgMobile}');
+    background-repeat: no-repeat;
+    background-position-x: 45%;
+    background-position-y: 45%;
+    background-color: var(--peach);
+    text-align: center;
+    height: 379px;
+    width: 327px;
+    border-radius: 15px;
+`;
+
+export const ContactInfoContainer = styled.div`
+
+    background-color: black;
+
+`;
+
+export const ContactNav = styled.nav`
+`;
+
