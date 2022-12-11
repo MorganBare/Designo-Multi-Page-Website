9626413d6e89399e35382f027e048b40444c879e
Morgan <morganjbare@gmail.com>
2022-12-11 19:04:24 +0100

    Added backgrounds plus overlay to project cards

--- src/components/projects/projectLinks/ProjectLinks.styled.js
+++ src/components/projects/projectLinks/ProjectLinks.styled.js
@@ -1,20 +1,37 @@
 import styled from "styled-components";
 
-export const ProjectLinksContainer = styled.div`
+import MobileApp from '../../../assets/home/mobile/image-app-design.jpg'
+import MobileGraphic from '../../../assets/home/mobile/image-graphic-design.jpg'
+import MobileWeb from '../../../assets/home/mobile/image-web-design.jpg'
 
+export const ProjectLinksContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5em;
+    margin-top: 6em;
 `;
 
 export const ProjectSquareWeb = styled.div`
     height: 250px;
     width: 327px;
+    background: url(${MobileWeb}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5));
+    background-blend-mode: overlay;
+    border-radius: 15px;
 `;
 
 export const ProjectSquareApp = styled.div`
     height: 250px;
     width: 327px;
+    background: url(${MobileApp}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5));
+    background-blend-mode: overlay;
+    border-radius: 15px;
 `;
 
 export const ProjectSquareGraphic = styled.div`
     height: 250px;
     width: 327px;
+    background: url(${MobileGraphic}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5));;
+    background-blend-mode: overlay;
+    border-radius: 15px;
 `;
