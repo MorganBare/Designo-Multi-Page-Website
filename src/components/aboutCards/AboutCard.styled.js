76caaacad2e78f0bb70a802fb72dd62ef22f36db
Morgan <morganjbare@gmail.com>
2022-12-18 11:49:05 +0100

    About cards styled for tablet

--- src/components/aboutCards/AboutCard.styled.js
+++ src/components/aboutCards/AboutCard.styled.js
@@ -12,21 +12,42 @@ import DesktopReal from '../../assets/about/desktop/image-world-class-talent.jpg
 
 export const CardContainer = styled.div`
     text-align: center;
+
+    @media screen and (min-width:760px) {
+        width: 689px;
+        border-radius: 15px;
+        margin: 6rem auto 4rem auto;
+    }
 `;
 
 export const ImgDiv = styled.div`
     height: 320px;
     width: 100%;
+
 `;
 
 export const ImgDivTalent = styled(ImgDiv)`
     background: no-repeat url(${MobileTalent});
     background-size: cover;
+
+    @media screen and (min-width:760px) {
+
+        background: no-repeat url(${TabletTalent});
+        border-radius: 15px 15px 0 0;
+
+    }
 `;
 
 export const ImgDivReal = styled(ImgDiv)`
     background: no-repeat url(${MobileReal});
     background-size: cover;
+
+    @media screen and (min-width:760px) {
+
+        background: no-repeat url(${TabletReal});
+        border-radius: 15px 15px 0 0;
+
+    }
 `;
 
 export const CardContent = styled.div`
@@ -34,13 +55,18 @@ export const CardContent = styled.div`
     height: 545px;
     background: no-repeat url(${bgPattern});
     background-color: ${({theme}) => theme.colors.cream};
-    background-position-x: 0px;
-    background-position-y: ;
 
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    @media screen and (min-width:760px) {
+        border-radius: 0 0 15px 15px;
+        height: 416px;
+        background-position-x:90px;
+        background-position-y:-208px;
+    }
 
     h1 {
         font-weight: ${({theme}) => theme.fontWeight.medium};
@@ -49,12 +75,20 @@ export const CardContent = styled.div`
         font-size: ${({theme}) => theme.fontSize.h1};
         color: ${({theme}) => theme.colors.peach};
         padding-bottom: 1.5rem;
+
+        @media screen and (min-width:760px) {
+        font-size: 48px;
+        }
     }
 
     p{
         width: 340px;
         margin: 0 auto;
         line-height: ${({theme}) => theme.lineHeight.small};
+
+        @media screen and (min-width:760px) {
+            width: 572px;
+        }
 
         &:first-of-type {
             padding-bottom: 1.5rem;
