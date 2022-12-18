94f062f94244168db5896e65dde57be3033637a3
Morgan <morganjbare@gmail.com>
2022-12-18 09:17:42 +0100

    about hero styled for tablet

--- src/components/heros/about/HeroAbout.styled.js
+++ src/components/heros/about/HeroAbout.styled.js
@@ -4,11 +4,18 @@ import MobileHero from '../../../assets/about/mobile/image-about-hero.jpg'
 import TabletHero from '../../../assets/about/tablet/image-about-hero.jpg'
 import DesktopHero from '../../../assets/about/desktop/image-about-hero.jpg'
 import MobileSVG from '../../../assets/about/mobile/bg-pattern-hero-about-mobile.svg'
+import DesktopSVG from '../../../assets/about/desktop/bg-pattern-hero-about-desktop.svg'
 
 
 export const HeroAboutContainer = styled.div`
 
     margin-bottom: 20rem;
+
+    @media screen and (min-width:760px) {
+        width: 689px;
+        border-radius: 15px;
+        margin: auto;
+    }
 
 `;
 
@@ -18,6 +25,12 @@ export const AboutImg = styled.div`
     width: 100%;
     background: no-repeat url(${MobileHero});
     background-size: cover;
+
+    @media screen and (min-width:760px) {
+        background: no-repeat url(${TabletHero});
+        background-size: cover;
+        border-radius: 15px 15px 0 0;
+    }
 `;
 
 export const AboutContent = styled.div`
@@ -31,18 +44,38 @@ export const AboutContent = styled.div`
     justify-content: center;
     align-items: center;
 
+    h1 {
+        font-weight: ${({theme}) => theme.fontWeight.medium};
+        line-height: ${({theme}) => theme.lineHeight.medium};
+        padding-bottom: 1rem;
+        font-size: ${({theme}) => theme.fontWeight.h1};
+
+        @media screen and (min-width:760px) {
+            font-size: 48px;
+            padding-bottom: 2rem;
+        }
+    }
+
+    p{
+        max-width: 340px;
+
+        @media screen and (min-width:760px) {
+            max-width: 573px;
+        }
+    }
+
     background: no-repeat url(${MobileSVG});
     background-color: ${({theme}) => theme.colors.peach};
     background-position-x: -215px;
     background-position-y: -140px;
 
-    h1 {
-        font-weight: ${({theme}) => theme.fontWeight.medium};
-        line-height: ${({theme}) => theme.lineHeight.medium};
-        padding-bottom: 1rem;
+    @media screen and (min-width:760px) {
+        border-radius: 0 0 15px 15px;
+        height: 312px;
+        background: no-repeat url(${DesktopSVG});
+        background-color: ${({theme}) => theme.colors.peach};
+        background-position-x: -115px;
+        background-position-y: -437px;
     }
 
-    p{
-        max-width: 340px;
-    }
 `;
