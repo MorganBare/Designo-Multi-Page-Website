9c7d9671e96967bbf016c9390a29d38763b1e517
Morgan <morganjbare@gmail.com>
2022-12-21 05:15:37 +0100

    Location pages styled for mobile

--- src/components/locations/Locations.styled.js
+++ src/components/locations/Locations.styled.js
@@ -9,11 +9,13 @@ import DesktopCA from '../../assets/locations/desktop/image-map-canada.png'
 import TabletUk from  '../../assets/locations/tablet/image-map-uk.png'
 import DesktopUk from  '../../assets/locations/desktop/image-map-united-kingdom.png'
 
+import Mobilebg from '../../assets/shared/desktop/bg-pattern-three-circles.svg'
 
 export const LocationsCotainer = styled.div``;
 
 export const SingleLocation = styled.div`
     text-align: center;
+    margin-bottom: 2rem;
 `;
 
 export const Map = styled.div`
@@ -38,6 +40,30 @@ export const MapUK = styled(Map)`
     background-size: cover;
 `;
 
-export const Address = styled.div``;
+export const Address = styled.div`
 
-export const AddressContent = styled.div``;
+    height: 394px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background: no-repeat url(${Mobilebg});
+    background-color: ${({theme}) => theme.colors.cream};
+
+    h2 {
+        color: ${({theme}) => theme.colors.peach};
+        font-size: ${({theme}) => theme.fontSize.h2};
+        font-weight: ${({theme}) => theme.fontWeight.medium};
+    }
+`;
+
+export const AddressContent = styled.div`
+    div {
+        p:first-of-type{
+            font-weight: ${({theme}) => theme.fontWeight.large};
+        }
+
+        padding-top: 1.5rem;
+    }
+`;
