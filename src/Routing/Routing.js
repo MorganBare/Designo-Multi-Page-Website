29bfa4605a714ab2b7f0063043074939c7527ec4
Morgan <morganjbare@gmail.com>
2022-12-31 10:48:23 +0100

    Revert "Attempting to fix a refresh bug on the deployed site in the routing"
    This reverts commit 2b41e4b4da4edf574aa1204be743b6cdb96cc4d5.

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 import { About, AppDesign, Contact, GraphicDesign, Locations, WebDesign, Home} from "../pages"
 import { MobileNavigation, Navigation } from "../components"
 import {ContactLayout, MainLayout} from "../layouts"
@@ -9,20 +8,20 @@ import ScrollToTop from "./ScrollToTop";
 const Routing = () => {
 
     return (
-        <HashRouter basename="/">
-                <ScrollToTop/>
-                <Navigation/>
-                <MobileNavigation/>
-                <Routes>
-                    <Route path="/" element={<MainLayout><Home /></MainLayout>} />
-                    <Route path="/App-Design" element={<MainLayout><AppDesign /></MainLayout>} />
-                    <Route path="/Contact" element={<ContactLayout><Contact /></ContactLayout>} />
-                    <Route path="/About" element={<MainLayout><About /></MainLayout>} />
-                    <Route path="/Graphic-Design" element={<MainLayout><GraphicDesign /></MainLayout>} />
-                    <Route path="/Locations" element={<MainLayout><Locations /></MainLayout>} />
-                    <Route path="/Web-Design" element={<MainLayout><WebDesign /></MainLayout>} />
-                </Routes>
-        </HashRouter>
+        <Router>
+            <ScrollToTop/>
+            <Navigation/>
+            <MobileNavigation/>
+            <Routes>
+                <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+                <Route path="/App-Design" element={<MainLayout><AppDesign /></MainLayout>} />
+                <Route path="/Contact" element={<ContactLayout><Contact /></ContactLayout>} />
+                <Route path="/About" element={<MainLayout><About /></MainLayout>} />
+                <Route path="/Graphic-Design" element={<MainLayout><GraphicDesign /></MainLayout>} />
+                <Route path="/Locations" element={<MainLayout><Locations /></MainLayout>} />
+                <Route path="/Web-Design" element={<MainLayout><WebDesign /></MainLayout>} />
+            </Routes>
+        </Router>
     )
 }
 
