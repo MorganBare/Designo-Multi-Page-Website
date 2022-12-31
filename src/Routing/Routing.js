2b41e4b4da4edf574aa1204be743b6cdb96cc4d5
Morgan <morganjbare@gmail.com>
2022-12-31 10:29:20 +0100

    Attempting to fix a refresh bug on the deployed site in the routing

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { About, AppDesign, Contact, GraphicDesign, Locations, WebDesign, Home} from "../pages"
 import { MobileNavigation, Navigation } from "../components"
 import {ContactLayout, MainLayout} from "../layouts"
@@ -8,20 +9,20 @@ import ScrollToTop from "./ScrollToTop";
 const Routing = () => {
 
     return (
-        <Router>
-            <ScrollToTop/>
-            <Navigation/>
-            <MobileNavigation/>
-            <Routes>
-                <Route path="/" element={<MainLayout><Home /></MainLayout>} />
-                <Route path="/App-Design" element={<MainLayout><AppDesign /></MainLayout>} />
-                <Route path="/Contact" element={<ContactLayout><Contact /></ContactLayout>} />
-                <Route path="/About" element={<MainLayout><About /></MainLayout>} />
-                <Route path="/Graphic-Design" element={<MainLayout><GraphicDesign /></MainLayout>} />
-                <Route path="/Locations" element={<MainLayout><Locations /></MainLayout>} />
-                <Route path="/Web-Design" element={<MainLayout><WebDesign /></MainLayout>} />
-            </Routes>
-        </Router>
+        <HashRouter basename="/">
+                <ScrollToTop/>
+                <Navigation/>
+                <MobileNavigation/>
+                <Routes>
+                    <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+                    <Route path="/App-Design" element={<MainLayout><AppDesign /></MainLayout>} />
+                    <Route path="/Contact" element={<ContactLayout><Contact /></ContactLayout>} />
+                    <Route path="/About" element={<MainLayout><About /></MainLayout>} />
+                    <Route path="/Graphic-Design" element={<MainLayout><GraphicDesign /></MainLayout>} />
+                    <Route path="/Locations" element={<MainLayout><Locations /></MainLayout>} />
+                    <Route path="/Web-Design" element={<MainLayout><WebDesign /></MainLayout>} />
+                </Routes>
+        </HashRouter>
     )
 }
 
